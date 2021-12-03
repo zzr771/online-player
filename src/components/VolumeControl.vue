b00217248a0c573b01dc294c08f301cac11857f0
Ray <771446555@qq.com>
2021-12-03 16:03:52 +0800

    音量控制: 完全百分比

--- src/components/VolumeControl.vue
+++ src/components/VolumeControl.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="volume-control">
-    <i class="iconfont icon-zuidayinliang"></i>
-    <div class="progress-bar" draggable="false">
+    <i class="iconfont icon-zuidayinliang" @click="mute" ref="volumeIcon"></i>
+    <div class="progress-bar" ref="progressBar">
       <div class="track"></div>
       <!-- slider左侧红色轨道 -->
       <div class="track-left" ref="trackLeft"></div>
@@ -13,6 +13,9 @@
           都是定位元素,当指针位于它们上方时, offsetX的值就变成了指针相对它们的坐标
         为了排除这一干扰,就要用一个遮罩层把下面的都遮住, 让offsetX始终返回指针相对
           遮罩层的坐标
+        而遮罩层要比轨道宽一些.不然在左侧,仅靠拖拽难以把slider拖到percent为0的位置;
+          在右侧,slider到达轨道右边尽头后从mask后面露出来了一半的宽度,鼠标如果点击
+          到了露出来的这一半再拖动, 就触发了浏览器自带的元素拖动, 无法改变音量了 
        -->
       <div class="mask" ref="mask"></div>
     </div>
@@ -23,16 +26,17 @@
 import { ref, onMounted, watch } from "vue"
 export default {
   setup() {
-    // slider的X坐标相对轨道总长度的百分比
-    let percent = ref(30)
+    // 此组件的核心变量. slider的X坐标相对轨道总长度的百分比,也即音量百分比.
+    let percent = ref(0)
     const slider = ref(null)
     const trackLeft = ref(null)
     const mask = ref(null)
+    const progressBar = ref(null)
     onMounted(() => {
       mask.value.onmousedown = () => {
-        mask.value.onmousemove = mouseMove
-        // 鼠标仅单击时也要移动slider, 所以执行一次mouseMove()
-        mouseMove()
+        mask.value.onmousemove = computePercent
+        // 鼠标仅单击时也要移动slider, 所以执行一次computePercent
+        computePercent()
       }
       mask.value.onmouseup = () => {
         mask.value.onmousemove = null
@@ -43,47 +47,89 @@ export default {
         所以当onmouseleave事件触发, 一样要解绑
       */
       mask.value.onmouseleave = () => {
-        mask.value.onmousemove = null
+        setTimeout(() => {
+          mask.value.onmousemove = null
+        }, 200)
       }
       // 初始化slider的位置和红色轨道的长度
-      moveSlider(0, percent)
+      percent.value = 70
     })
 
-    let timer = null //节流
-    function mouseMove() {
-      if (timer) {
-        return
-      }
+    // 计算percent
+    function computePercent() {
+      const totalWidth = progressBar.value.clientWidth
       const event = window.event
       let mouseX = event.offsetX
-      timer = setTimeout(() => {
-        // 改变slider位置和红色轨道的长度
-        moveSlider(mouseX)
-        // 重新计算百分比
-        let width = mask.value.clientWidth
-        percent.value = Math.round((mouseX / width) * 100)
-        console.log(percent.value)
-        timer = null
-      }, 10)
-    }
-
-    // 根据mouseX或percent的值改变slider的位置. percent优先级更高
-    function moveSlider(mouseX, percent) {
-      let width = mask.value.clientWidth
-      if (percent) {
-        mouseX = (percent.value / 100) * width
+      // mask左侧超出轨道5px,这里减去
+      percent.value = ((mouseX - 5) / totalWidth) * 100
+      // 检查一下percent是否小于0或者大于100
+      if (percent.value < 0) {
+        percent.value = 0
+      } else if (percent.value > 100) {
+        percent.value = 100
       }
-      slider.value.style.left = mouseX - 6 + "px"
-      trackLeft.value.style.width = mouseX - 6 + "px"
+      // console.log(percent.value)
     }
 
-    // 监视percent, 根据值的大小改变音量图标
-    watch(percent, (newValue) => {})
+    // 监视percent, 根据值的大小移动slider,以及改变音量图标
+    const volumeIcon = ref(null)
+    watch(percent, (newValue) => {
+      moveSlider(newValue)
+
+      // 修改音量图标
+      switch (true) {
+        case newValue === 0:
+          changeIconTo("icon-guanbishengyin")
+          break
+        case newValue <= 33:
+          changeIconTo("icon-yinliang")
+          break
+        case newValue > 33 && newValue <= 66:
+          changeIconTo("icon-zhongdengyinliang")
+          break
+        case newValue > 66:
+          changeIconTo("icon-zuidayinliang")
+          break
+      }
+    })
+    function changeIconTo(iconName) {
+      volumeIcon.value.classList.remove(
+        "icon-guanbishengyin",
+        "icon-zhongdengyinliang",
+        "icon-zuidayinliang",
+        "icon-yinliang"
+      )
+      volumeIcon.value.classList.add(iconName)
+    }
+
+    // 根据percent的值改变slider的位置
+    function moveSlider(percent) {
+      const totalWidth = progressBar.value.clientWidth
+      // slider自身有宽度,取其宽度的一半作为修正值. 每次都让slider向左修正该值,这样slider的中心就和鼠标位置重合了
+      const correction = (slider.value.clientWidth / 2 / totalWidth) * 100
+      let position = percent - correction
+      slider.value.style.left = position + "%"
+      // 当position为负数时,红色轨道的width不能为负数,最小是0
+      trackLeft.value.style.width = position < 0 ? "0%" : position + "%"
+    }
+
+    let previousPercent = 0
+    function mute() {
+      if (percent.value !== 0) {
+        previousPercent = percent.value
+        percent.value = 0
+      } else {
+        percent.value = previousPercent
+      }
+    }
 
     return {
       slider,
       trackLeft,
       mask,
+      progressBar,
+      mute,
+      volumeIcon,
     }
   },
 }
@@ -131,38 +177,9 @@ export default {
       position: absolute;
       top: 0;
       bottom: 0;
-      left: 0;
-      right: 0;
+      left: -5px;
+      right: -10px;
     }
   }
-
-  // // 都有效
-  // input[type="range"] {
-  //   // 去除滑动条默认样式
-  //   -webkit-appearance: none;
-  //   -moz-appearance: none;
-  //   width: 150px;
-  //   border-radius: 10px; /*这个属性设置使填充进度条时的图形为圆角*/
-  // }
-  // // 去除默认的获得焦点时的样式
-  // input[type="range"]:focus {
-  //   outline: none;
-  // }
-  // // 滑块的样式 firefox有效 chrome无效
-  // input[type="range"]::-webkit-slider-thumb,
-  // input[type="range"]::-moz-range-thumb {
-  //   width: 12px;
-  //   height: $width;
-  //   border-radius: 50%;
-  //   border: none;
-  //   background: @theme-color;
-  // }
-  // // 轨道的样式  firefox有效 chrome无效
-  // input[type="range"]::-webkit-slider-runnable-track,
-  // input[type="range"]::-moz-range-track {
-  //   height: 2px;
-  //   border-radius: 1px; /*将轨道设为圆角的*/
-  //   background-color: @theme-color;
-  // }
 }
 </style>
