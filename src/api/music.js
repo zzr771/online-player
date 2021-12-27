c55946b6ec3938db5019c26c9678dc331d4fef64
Ray <771446555@qq.com>
2021-12-28 02:01:35 +0800

    添加: 请求相关歌单和相似歌曲

--- src/api/music.js
+++ src/api/music.js
@@ -2,3 +2,5 @@ import ajax from "@/api/ajax"
 
 export const reqLyric = (songId) => ajax("/lyric", { id: songId })
 export const reqArtist = (ArtistId) => ajax("/artists", { id: ArtistId })
+export const reqRelevantPlayLists = (songId) => ajax("/simi/playlist", { id: songId })
+export const reqSimiSongs = (songId) => ajax("/simi/song", { id: songId })
