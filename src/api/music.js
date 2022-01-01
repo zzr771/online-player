187b6f8057c9624eaf4dc60de1b7b26788310e07
Ray <771446555@qq.com>
2022-01-01 15:47:55 +0800

    添加api

--- src/api/music.js
+++ src/api/music.js
@@ -1,6 +1,14 @@
 import ajax from "@/api/ajax"
 
+// 歌词
 export const reqLyric = (songId) => ajax("/lyric", { id: songId })
+// 作者
 export const reqArtist = (ArtistId) => ajax("/artists", { id: ArtistId })
+// 相关歌单(播放器页面)
 export const reqRelevantPlayLists = (songId) => ajax("/simi/playlist", { id: songId })
+// 相似歌曲(播放器页面)
 export const reqSimiSongs = (songId) => ajax("/simi/song", { id: songId })
+// 新歌速递(最新音乐页面)
+export const reqTopSongs = (type) => ajax("/top/song", { type })
+// 歌曲详情
+export const reqSongDetail = (id) => ajax("/song/detail", { ids: id })
--- src/api/mv.js
+++ src/api/mv.js
@@ -3,4 +3,4 @@ import ajax from "./ajax"
 export const reqMvDetail = (id) => ajax("/mv/detail", { mvid: id })
 export const reqMvUrl = (id) => ajax("/mv/url", { id: id })
 export const reqSimiMvs = (id) => ajax("/simi/mv", { mvid: id })
-export const reqAllMvs = (params) => ajax("/mv/all", { params })
+export const reqAllMvs = (params) => ajax("/mv/all", params)
