0eaf393a4a3b97c5c6dd0a24c6d4282ea8e377b1
Ray <771446555@qq.com>
2022-01-14 23:06:42 +0800

    修改配置以部署

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@
 const path = require("path")
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/online-player/" : "/",
   lintOnSave: false,
   devServer: {
     open: true,
