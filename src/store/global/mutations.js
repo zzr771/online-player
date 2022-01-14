91d81307337306bc7129fd815d0159107c2ebaf4
Ray <771446555@qq.com>
2022-01-14 14:17:37 +0800

    添加错误信息和全局loading

--- src/store/global/mutations.js
+++ src/store/global/mutations.js
@@ -9,4 +9,29 @@ export default {
       state.previousPositions.push({ routeName, position })
     }
   },
+  setreqErrMsg(state, { msg }) {
+    state.reqErrMsg = msg
+  },
+
+  // 显示loading
+  showLoading(state) {
+    state.showLoading = 1
+  },
+
+  // 根据value值, 决定是否隐藏loading
+  hideLoading(state, { value }) {
+    // 如果是一般请求
+    if (value === 1) {
+      state.showLoading = 0
+    }
+    // 如果是"discovery"页面的请求
+    else {
+      state.showLoadingCount += value
+      // 当4个请求都完成时
+      if (state.showLoadingCount === 1) {
+        state.showLoading = 0
+        state.showLoadingCount = 0
+      }
+    }
+  },
 }
