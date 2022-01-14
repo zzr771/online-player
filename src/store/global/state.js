db5895ea88ce5049e1f1a7260b7762f7b7ec1531
Ray <771446555@qq.com>
2022-01-14 22:18:21 +0800

    修改注释

--- src/store/global/state.js
+++ src/store/global/state.js
@@ -4,7 +4,7 @@ export default {
   // 请求出错时的错误信息
   reqErrMsg: "",
 
-  // 是否显示loading图标  值为1则显示loading,值为0则隐藏
+  // 是否显示全局loading(转圈的加载中图标)  值为1则显示loading,值为0则隐藏
   showLoading: 0,
   // "discovery"页面请求的计数器, 每完成一个请求, 加0.25. 等于1时说明全部请求都完成
   showLoadingCount: 0,
