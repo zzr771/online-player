bb61b7a9c282791c52bdb92ed81bd0e9faa8870f
Ray <771446555@qq.com>
2022-01-14 14:32:18 +0800

    把异步请求交给父路由

--- src/pages/Search/routes/MVs.vue
+++ src/pages/Search/routes/MVs.vue
@@ -1,71 +1,62 @@
 <template>
   <div class="mvs">
     <div class="mvs-wrapper">
-      <!-- <MVCard v-for="(item, index) in 15" :key="index"></MVCard> -->
+      <MVCard v-for="(mv, index) in mvs" :key="index" :mv="mv"></MVCard>
     </div>
-    <Pagination :totalPageNum="10"></Pagination>
+    <Pagination :totalPageNum="totalPage"></Pagination>
   </div>
 </template>
 
 <script>
-import MVCard from "@/components/MVCard"
 import Pagination from "@/components/Pagination"
-import { ref, reactive, provide, computed, watch, inject } from "vue"
-import { reqSearch } from "@/api/search"
-import { parseTime, simplifyPlayCount } from "@/utils/common"
+import MVCard from "@/components/MVCard"
+import { ref, reactive, provide, watch, inject } from "vue"
 
 const LIMIT = 20
 export default {
-  props: {
-    keyword: String,
-  },
-  setup(props) {
-    let currentPage = ref(1)
-    function updateCurrentPage(page) {
-      currentPage.value = page
-    }
-    provide("updateCurrentPage", updateCurrentPage)
-    watch(currentPage, () => {
-      document.getElementById("search").scrollIntoView({ behavior: "smooth" })
-      getmvs(props.keyword)
-    })
-
-    let keyword = computed(() => props.keyword)
-    watch(keyword, (newValue) => {
-      getmvs(newValue)
-    })
+  setup() {
+    let data = inject("data")
 
     let mvs = reactive([])
     let totalPage = ref(1)
-    let resultsCount = inject("resultsCount")
-    async function getmvs(keyword) {
-      let params = {
-        keywords: keyword,
-        limit: LIMIT + 1,
-        offset: LIMIT * (currentPage.value - 1),
-        type: 1004,
+    // 当前页码, 传递给Pagination组件
+    let currentPage = ref(1)
+    provide("currentPage", currentPage)
+
+    // 监视data["search-mvs"],即搜索到的mv
+    watch(
+      () => data["search-mvs"],
+      (newValue) => {
+        // 如果还没有数据或者请求错误
+        if (!newValue.result || newValue.code !== 200) return
+
+        let result = newValue.result
+        totalPage.value = Math.ceil(result.mvCount / LIMIT)
+
+        mvs.length = 0
+        mvs = Object.assign(mvs, result.mvs)
       }
-      let _mvs = await reqSearch(params)
+    )
 
-      _mvs = _mvs.result
-      resultsCount.value = _mvs.songCount
-      totalPage.value = Math.ceil(_mvs.songCount / LIMIT)
+    // 监视搜索关键词,如果变化了,就把currentPage重置为1
+    watch(
+      () => data.keyword,
+      (newValue, oldValue) => {
+        if (newValue !== oldValue) {
+          currentPage.value = 1
+        }
+      }
+    )
 
-      // 返回的搜索结果可能大于30个,多余的截去
-      _mvs = _mvs.mvs.slice(0, 30)
-      mvs.length = 0
-      mvs = Object.assign(mvs, _mvs)
-    }
-    // 页面第一次打开,自动发送请求
-    if (props.keyword.length) {
-      getmvs(props.keyword)
-    }
+    // 页码改变,通知search,然后search会发送新的请求
+    let uploadCurrentPage = inject("uploadCurrentPage")
+    watch(currentPage, (newValue) => {
+      uploadCurrentPage(newValue)
+    })
+
     return { mvs, totalPage }
   },
-  components: {
-    Pagination,
-    MVCard,
-  },
+  components: { Pagination, MVCard },
 }
 </script>
 
