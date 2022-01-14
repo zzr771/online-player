d4d96e5fa012c663a5651db266579ae26e26dd5e
Ray <771446555@qq.com>
2022-01-14 14:30:40 +0800

    使用后端数据

--- src/pages/RecLists/RecLists.vue
+++ src/pages/RecLists/RecLists.vue
@@ -1,26 +1,25 @@
 // "推荐歌单"一级路由页
 <template>
   <div class="rec-lists">
-    <div class="top-card">
-      <img src="./images/top.jpg" alt="" />
+    <div class="top-card" v-if="playlists.HQPlaylist.id">
+      <img v-lazy="genImgURL(playlists.HQPlaylist.coverImgUrl, 280)" alt="" />
       <div class="content">
         <div class="tag">精品歌单</div>
-        <p class="name">华语民谣 I 孤独的心诠释诗意和远方</p>
+        <p class="name">{{ playlists.HQPlaylist.name }}</p>
         <p class="description">
-          民谣, 一听就是一个故事; 民谣, 唱的人普通, 听的人平凡; 民谣, 喜欢那种感同身受 热爱那些温柔辞藻.
-          用歌曲说话, 用歌曲讲故事, 因为被赋予旋律, 所以才更容易打动人心. 简单的旋律和歌词,
-          也可以传达出最饱含诗意的深情. 有时候, 民谣像是唱着漫长的一生, 却又像只是讲了某天放学后,
-          暗恋少年阳光下灿烂的笑脸. 在民谣里回忆过去, 怀念过去的故事, 想念那些无法再见的人; 在民谣里憧憬未来,
-          坚信有一天终将会抵达的远方. 因为有记忆, 我们变得丰满; 因为有向往, 我们才会一往无前. 即使有一天,
-          我们白发苍苍, 但回首一生, 有那么多丰富的回忆和故事, 足矣. 愿孤独的你有诗的态度, 也有当下的远方.
+          {{ playlists.HQPlaylist.description }}
         </p>
+      </div>
+      <div class="background">
+        <div class="mask"></div>
+        <img v-lazy="genImgURL(playlists.HQPlaylist.coverImgUrl, 1000, 200)" alt="" />
       </div>
     </div>
     <TabsBasic></TabsBasic>
-    <div class="lists-wrapper">
-      <ListCard v-for="(item, index) in 20" :key="index"></ListCard>
+    <div class="lists-wrapper" v-if="playlists.playlists.length">
+      <ListCard v-for="(playlist, index) in playlists.playlists" :key="index" :playlist="playlist"></ListCard>
     </div>
-    <Pagination :totalPageNum="20"></Pagination>
+    <Pagination :totalPageNum="totalPageNum"></Pagination>
   </div>
 </template>
 
@@ -28,29 +27,79 @@
 import TabsBasic from "@/components/TabsBasic"
 import ListCard from "@/components/ListCard"
 import Pagination from "@/components/Pagination"
-import { reactive, provide } from "vue"
+import { ref, reactive, provide, watch } from "vue"
+import { reqPlaylists, reqHQPlaylists } from "@/api/music"
+import { genImgURL } from "@/utils/common"
+
+const LIMIT = 25
 export default {
-  components: { TabsBasic, ListCard, Pagination },
   setup() {
+    let playlists = reactive({ HQPlaylist: {}, playlists: [] })
+    // 总页数
+    let totalPageNum = ref(1)
+    // 页码
+    let currentPage = ref(1)
+    provide("currentPage", currentPage)
+    watch(currentPage, () => {
+      getPlaylists()
+      document.querySelector(".w").scrollIntoView({ behavior: "smooth" })
+    })
+
     let tabs = reactive([
-      { name: "全部", on: false },
+      { name: "全部", on: true },
       { name: "欧美", on: false },
       { name: "华语", on: false },
+      { name: "日语", on: false },
       { name: "流行", on: false },
       { name: "说唱", on: false },
       { name: "摇滚", on: false },
       { name: "民谣", on: false },
-      { name: "电子", on: false },
       { name: "轻音乐", on: false },
       { name: "影视原声", on: false },
-      { name: "ACG", on: false },
       { name: "怀旧", on: false },
       { name: "治愈", on: false },
-      { name: "旅行", on: false },
     ])
     provide("tabs", tabs)
-    return {}
+    let tabOn = {}
+    watch(
+      tabs,
+      (newValue) => {
+        tabOn = newValue.find((tab) => {
+          return tab.on
+        })
+
+        getPlaylists()
+      },
+      { immediate: true }
+    )
+
+    //-------------------------------------获取歌单----------------------------
+    // 获取精品歌单
+    ;(async function getHQPlaylist() {
+      let result = await reqHQPlaylists()
+      if (result.code === 200) {
+        playlists.HQPlaylist = result.playlists[0]
+      }
+    })()
+
+    //获取普通歌单
+    async function getPlaylists() {
+      let params = {
+        cat: tabOn.name,
+        limit: LIMIT,
+        offset: LIMIT * (currentPage.value - 1),
+      }
+      let result = await reqPlaylists(params)
+      if (result.code === 200) {
+        playlists.playlists = []
+        playlists.playlists = result.playlists
+        totalPageNum.value = Math.ceil(result.total / LIMIT)
+      }
+    }
+
+    return { playlists, genImgURL, totalPageNum }
   },
+  components: { TabsBasic, ListCard, Pagination },
 }
 </script>
 
@@ -68,24 +117,7 @@ export default {
   padding: 16px;
   background-color: rgba(0, 0, 0, 0.5);
   overflow: hidden;
-  /* 
-    需求: 背景图模糊
-    backdrop-filter在chrome有效,但在火狐浏览器无效,需要手动开启该功能 
-    所以只能用filter来做. filter相当于加了一层遮罩在元素上面, 元素内部的所有东西都被影响
-    但是filter不仅作用于本元素,还作用于本元素的子元素
-      所以给父元素一个after伪子元素,在伪子元素内做背景模糊
-  */
-  &::after {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    width: 100%;
-    height: 100%;
-    background-image: url("./images/background.jpg");
-    background-size: cover;
-    filter: blur(20px) contrast(90%) brightness(0.7) drop-shadow(-15px -15px 1px rgba(0, 0, 0, 0.5));
-  }
-  img {
+  & > img {
     margin-right: 8px;
   }
   .content {
@@ -98,6 +130,7 @@ export default {
       color: @gold;
       border: 1px solid @gold;
       border-radius: 5px;
+      cursor: pointer;
     }
     .name {
       margin-bottom: 8px;
@@ -106,10 +139,45 @@ export default {
     }
     .description {
       height: 60px;
-      overflow: hidden;
-      text-overflow: ellipsis;
+      line-height: 15px;
       font-size: @font-size-sm;
       color: var(--font-color-grey-shallow);
+
+      display: -webkit-box;
+      overflow: hidden;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+      text-overflow: ellipsis;
+    }
+  }
+  /* 
+    需求: 背景图模糊
+    backdrop-filter在chrome有效,但在火狐浏览器无效,需要手动开启该功能 
+    所以只能用filter来做. filter相当于加了一层遮罩在元素上面, 元素内部的所有东西都被影响
+    但是filter不仅作用于本元素,还作用于本元素的子元素
+      所以给父元素一个after伪子元素,在伪子元素内做背景模糊
+      或者单独创建一个背景层, 只对背景层使用blur
+  */
+  .background {
+    position: absolute;
+    z-index: -10;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    img {
+      position: absolute;
+      z-index: -10;
+      filter: blur(20px);
+    }
+    .mask {
+      position: absolute;
+      z-index: -9;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background-color: rgba(0, 0, 0, 0.5);
     }
   }
 }
