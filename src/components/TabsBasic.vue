91741c72eff5d6724bd376376801fd5dec7ce320
Ray <771446555@qq.com>
2022-01-14 14:25:41 +0800

    添加tab大小参数

--- src/components/TabsBasic.vue
+++ src/components/TabsBasic.vue
@@ -6,7 +6,8 @@
       :class="{ on: tab.on }"
       v-for="(tab, index) in tabs"
       :key="index"
-      @click="clickHandler(tab)"
+      @click="onClick(tab)"
+      :style="{ fontSize: `${fontSize}px` }"
       >{{ tab.name }}</span
     >
   </div>
@@ -15,20 +16,26 @@
 <script>
 import { inject } from "vue"
 export default {
+  props: {
+    fontSize: {
+      type: Number,
+      default: 12,
+    },
+  },
   setup() {
-    // tabs中的每个对象必须具有name和on属性
+    // 为了通信方便,该组件必须从父组件接收定义好的tabs数组. tabs中的每个对象必须具有name和on属性
     let tabs = inject("tabs")
 
     // 将被点击的tab的on属性变为true
     let previousOnTab = tabs[0]
-    function clickHandler(tab) {
+    function onClick(tab) {
       if (previousOnTab !== tab) {
         tab.on = true
         previousOnTab.on = false
         previousOnTab = tab
       }
     }
-    return { clickHandler, tabs }
+    return { onClick, tabs }
   },
 }
 </script>
