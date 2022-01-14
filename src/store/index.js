e9a4ee7f8a38e8e0bbc94a06598ee8cce2b3bb18
Ray <771446555@qq.com>
2022-01-14 14:17:06 +0800

    添加user模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex"
 import music from "./music/index"
 import global from "./global/index"
+import user from "./user/index"
 
 export default createStore({
   modules: {
     music,
     global,
+    user,
   },
 })
